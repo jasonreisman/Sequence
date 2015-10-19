e19a35f84e2301f03a2a03b4f9eab3864f36a0cf
Jason Reisman <jasonreisman@gmail.com>
2015-10-19 15:51:31 -0400

    - Support custom colors for steps - Support steps that only involve a single actor - Explicily setting top left corner of drawing - Example file is now client server

--- make_sequence.py
+++ make_sequence.py
@@ -11,6 +11,7 @@ class Colors:
 
 class Sequence:
 	def __init__(self, filename):
+		self.filename = filename
 		self.actors = []
 		self.actors_map = {}
 		self.actions = []
@@ -34,15 +35,20 @@ class Sequence:
 					self.actors.append(actor1)
 				key0 = self.actors_map[actor0]
 				key1 = self.actors_map[actor1]
-				self.actions.append((key0, key1, action))
+				color = Colors.black
+				if len(tokens) > 3:
+					color = tokens[3].strip()
+				self.actions.append((key0, key1, action, color))
 		# create drawing
-		self.step_size = (100, 50)
+		self.top_left = (50, 50)
+		self.step_size = (100, 25)
 		self.text_fudge = (0, 5)
-		self.width = (len(self.actors) + 1)*self.step_size[0]
-		self.height = (len(self.actions) + 2)*self.step_size[1]
+		self.width = self.top_left[0] + (len(self.actors) + 1)*self.step_size[0]
+		self.height = self.top_left[1] + (len(self.actions) + 1)*self.step_size[1]
 		self.drawing = svgwrite.Drawing('out.svg', size=(self.width, self.height))
 
 	def build(self):
+		self.create_header()
 		self.create_actors()
 		self.create_actions()
 		pass
@@ -50,9 +56,13 @@ class Sequence:
 	def to_string(self):
 		return self.drawing.tostring()
 
+	def create_header(self):
+			text = 'filename: %s' % self.filename
+			self.drawing.add(self.drawing.text(text, insert=(self.top_left[0], 0.5*self.top_left[1]), stroke='none', fill=Colors.black, font_family="Helevetica", font_size="8pt", text_anchor="start"))
+
 	def create_actors(self):
-		x = self.step_size[0]
-		y = self.step_size[1]
+		x = self.top_left[0]
+		y = self.top_left[1]
 		for name in self.actors:
 			self.drawing.add(self.drawing.text(name, insert=(x, y - self.text_fudge[1]), stroke='none', fill=Colors.black, font_family="Helevetica", font_size="8pt", text_anchor="middle"))
 			line = self.drawing.add(self.drawing.line((x, y), (x, self.height), stroke=Colors.gray, stroke_width=1))
@@ -60,22 +70,25 @@ class Sequence:
 			x += self.step_size[0]
 
 	def create_actions(self):
-		# create a marker objects
-		start_marker = self.drawing.marker(insert=(3,3), size=(10,10), orient='auto')
-		start_marker.add(self.drawing.circle((3, 3), r=3, fill=Colors.black))
-		self.drawing.defs.add(start_marker)
-		end_marker = self.drawing.marker(insert=(6,3), size=(10,10), orient='auto')
-		end_marker.add(self.drawing.path("M0,0 L0,7 L6,3 L0,0", fill=Colors.black))
-		self.drawing.defs.add(end_marker)
 		# add actions
-		y = 2*self.step_size[1]
-		for (a0, a1, act) in self.actions:
-			x0 = (a0 + 1)*self.step_size[0]
-			x1 = (a1 + 1)*self.step_size[0]
-			line = self.drawing.add(self.drawing.line((x0, y), (x1, y), stroke=Colors.black, stroke_width=1))
-			line['marker-start'] = start_marker.get_funciri()
-			line['marker-end'] = end_marker.get_funciri()
-			self.drawing.add(self.drawing.text(act, insert=(0.5*(x0 + x1), y - self.text_fudge[1]), stroke='none', fill=Colors.black, font_family="Helevetica", font_size="6pt", text_anchor="middle"))
+		y = self.top_left[1] + self.step_size[1]
+		for (a0, a1, act, color) in self.actions:
+			x0 = self.top_left[0] + a0*self.step_size[0]
+			x1 = self.top_left[0] + a1*self.step_size[0]
+			if a0 == a1:
+				self.drawing.add(self.drawing.circle((x0, y), r=2, fill=color))
+			else:
+				# create a marker objects
+				start_marker = self.drawing.marker(insert=(2,2), size=(10,10), orient='auto')
+				start_marker.add(self.drawing.circle((2,2), r=2, fill=color))
+				self.drawing.defs.add(start_marker)
+				end_marker = self.drawing.marker(insert=(6,3), size=(10,10), orient='auto')
+				end_marker.add(self.drawing.path("M0,0 L0,7 L6,3 L0,0", fill=color))
+				self.drawing.defs.add(end_marker)
+				line = self.drawing.add(self.drawing.line((x0, y), (x1, y), stroke=color, stroke_width=1))
+				line['marker-start'] = start_marker.get_funciri()
+				line['marker-end'] = end_marker.get_funciri()
+			self.drawing.add(self.drawing.text(act, insert=(0.5*(x0 + x1), y - self.text_fudge[1]), stroke='none', fill=color, font_family="Helevetica", font_size="6pt", text_anchor="middle"))
 			y += self.step_size[1]
 
 def usage():
