8589e45ad6a571a44524d4f9454fa3bb1e7162a1
Jason Reisman <jasonreisman@gmail.com>
2015-10-20 09:48:53 -0400

    - Reuse existing markers instead of creating new ones for each line. - Allow empty lines and lines starting with hashtags - Split long functions into multiple functions

--- make_sequence.py
+++ make_sequence.py
@@ -16,8 +16,26 @@ class Sequence:
 		self.actors_map = {}
 		self.actions = []
 		# read actions into file
+		self.parse_input_file(filename)
+		# create drawing
+		self.top_left = (50, 50)
+		self.step_size = (100, 25)
+		self.text_fudge = (0, 5)
+		self.width = self.top_left[0] + (len(self.actors) + 1)*self.step_size[0]
+		self.height = self.top_left[1] + (len(self.actions) + 1)*self.step_size[1]
+		self.drawing = svgwrite.Drawing(size=(self.width, self.height))
+		self.markers = {}
+
+	def parse_input_file(self, filename):
+		# read actions into file
 		with open(filename) as f:
 			for i, line in enumerate(f):
+				if len(line) == 0:
+					# skip empty lines
+					continue
+				if line[0] == '#':
+					# skip comments
+					continue
 				tokens = line.split(',')
 				assert len(tokens) >= 3, 'line %i has less than 3 tokens' % (i)
 				if len(tokens) < 3:
@@ -39,13 +57,6 @@ class Sequence:
 				if len(tokens) > 3:
 					color = tokens[3].strip()
 				self.actions.append((key0, key1, action, color))
-		# create drawing
-		self.top_left = (50, 50)
-		self.step_size = (100, 25)
-		self.text_fudge = (0, 5)
-		self.width = self.top_left[0] + (len(self.actors) + 1)*self.step_size[0]
-		self.height = self.top_left[1] + (len(self.actions) + 1)*self.step_size[1]
-		self.drawing = svgwrite.Drawing('out.svg', size=(self.width, self.height))
 
 	def build(self):
 		self.create_header()
@@ -70,6 +81,7 @@ class Sequence:
 			x += self.step_size[0]
 
 	def create_actions(self):
+		markers = {}
 		# add actions
 		y = self.top_left[1] + self.step_size[1]
 		for (a0, a1, act, color) in self.actions:
@@ -78,18 +90,29 @@ class Sequence:
 			if a0 == a1:
 				self.drawing.add(self.drawing.circle((x0, y), r=2, fill=color))
 			else:
-				# create a marker objects
-				start_marker = self.drawing.marker(insert=(2,2), size=(10,10), orient='auto')
-				start_marker.add(self.drawing.circle((2,2), r=2, fill=color))
-				self.drawing.defs.add(start_marker)
-				end_marker = self.drawing.marker(insert=(6,3), size=(10,10), orient='auto')
-				end_marker.add(self.drawing.path("M0,0 L0,7 L6,3 L0,0", fill=color))
-				self.drawing.defs.add(end_marker)
+				start_marker, end_marker = self.get_markers(color)
+				assert start_marker is not None
+				assert end_marker is not None
 				line = self.drawing.add(self.drawing.line((x0, y), (x1, y), stroke=color, stroke_width=1))
 				line['marker-start'] = start_marker.get_funciri()
 				line['marker-end'] = end_marker.get_funciri()
 			self.drawing.add(self.drawing.text(act, insert=(0.5*(x0 + x1), y - self.text_fudge[1]), stroke='none', fill=color, font_family="Helevetica", font_size="6pt", text_anchor="middle"))
 			y += self.step_size[1]
+
+	def get_markers(self, color):
+		# create or get marker objects
+		start_marker, end_marker = None, None
+		if color in self.markers:
+			start_marker, end_marker = self.markers[color]
+		else:
+			start_marker = self.drawing.marker(insert=(2,2), size=(10,10), orient='auto')
+			start_marker.add(self.drawing.circle((2,2), r=2, fill=color))
+			self.drawing.defs.add(start_marker)
+			end_marker = self.drawing.marker(insert=(6,3), size=(10,10), orient='auto')
+			end_marker.add(self.drawing.path("M0,0 L0,7 L6,3 L0,0", fill=color))
+			self.drawing.defs.add(end_marker)	
+			self.markers[color] = (start_marker, end_marker)
+		return start_marker, end_marker
 
 def usage():
 	print 'Usage: ./make_sequence.py <in filename> > <out filename>'
